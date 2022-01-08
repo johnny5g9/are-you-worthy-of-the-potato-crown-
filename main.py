a113bfcff7a06f5ea84245e1d67d24ef45bd147c
johnnygroissl11 <92061927+johnny5g9@users.noreply.github.com>
2022-01-08 18:21:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+choice = input("do you like potatos?")
+if choice == "yes":
+  print("you are worthy of the potato crown")
+if choice == "no":
+    print("YOU SHALL DIE")
+if choice not in ["yes", "no"]:
+  print("its a yes or no question")
